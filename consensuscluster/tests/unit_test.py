de492574b694348e98307903f1deb8a4fe2ec6cb
Vicram Rajagopalan <vicram.r@hotmail.com>
2019-05-14 23:35:55 -0500

    Begin refactor: made vars for test_get_ax_size global. Refactoring test_get_ax_size to be split across multiple test_* functions. The first step was to make everything shared among the tests global. Judging from the pytest documentation, this should work fine.

--- consensuscluster/tests/unit_test.py
+++ consensuscluster/tests/unit_test.py
@@ -6,6 +6,47 @@ from matplotlib import pyplot as plt
 
 from consensuscluster.plotutils import _get_ax_size
 
+
+# *** BEGIN helper objects/tests for _get_ax_size ***
+MAX_ERROR = .1  # We're willing to tolerate at most 10% error
+
+
+def _check_answer(true_width_pix, true_height_pix,
+                 approx_width_pix, approx_height_pix):
+    """Helper function for testing _get_ax_size.
+
+    Asserts that the answer found by _get_ax_size is within the
+    acceptable margin of error of the true answer (or at least,
+    whatever we're considering the true answer to be).
+    :param true_width_pix: True width of the Axes, in pixels.
+    :param true_height_pix: True height of the Axes, in pixels.
+    :param approx_width_pix: The width approximation returned by
+    _get_ax_size.
+    :param approx_height_pix: The height approximation returned by
+    _get_ax_size.
+    :return: nothing.
+    """
+    width_lower_bound = true_width_pix - MAX_ERROR * true_width_pix
+    width_upper_bound = true_width_pix + MAX_ERROR * true_width_pix
+    height_lower_bound = true_height_pix - MAX_ERROR * true_height_pix
+    height_upper_bound = true_height_pix + MAX_ERROR * true_height_pix
+
+    assert width_lower_bound <= approx_width_pix <= width_upper_bound
+    assert height_lower_bound <= approx_height_pix <= height_upper_bound
+
+
+figsizes = [
+    (1, 1),
+    (3, 3),
+    (4, 4),
+    (4, 9),
+    (.87, .4445),
+    (5.829, 1)
+]
+
+dpis = [100, 43.793]
+
+
 def test_get_ax_size():
     """
     Tests that _get_ax_size returns an answer that is within a
@@ -13,35 +54,15 @@ def test_get_ax_size():
 
     Because I just grabbed this function off of stackoverflow without
     any evidence that it was actually correct, it's important to really
-    test the bejesus out of this function.
+    test the bejesus out of it.
     :return: nothing.
     """
-    max_error = .1  # We're willing to tolerate at most 10% error
-
-    def check_answer(true_width_pix, true_height_pix,
-                     approx_width_pix, approx_height_pix):
-        width_lower_bound = true_width_pix - max_error * true_width_pix
-        width_upper_bound = true_width_pix + max_error * true_width_pix
-        height_lower_bound = true_height_pix - max_error * true_height_pix
-        height_upper_bound = true_height_pix + max_error * true_height_pix
-
-        assert width_lower_bound <= approx_width_pix <= width_upper_bound
-        assert height_lower_bound <= approx_height_pix <= height_upper_bound
-
-    figsizes = [
-        (1, 1),
-        (3, 3),
-        (4, 4),
-        (4, 9),
-        (.87, .4445),
-        (5.829, 1)
-    ]
-    dpis = [100, 43.793]
 
     for figsize in figsizes:
         (width, height) = figsize  # True values, in inches
         for dpi in dpis:
-            width_pix = width * dpi  # True value, in pixels
+            # True values, in pixels
+            width_pix = width * dpi
             height_pix = height * dpi
 
             # First: try figure.Figure
@@ -49,7 +70,7 @@ def test_get_ax_size():
             ax1 = fig1.gca()
             # ax1 should cover the entire figure.
             (approx_width_1, approx_height_1) = _get_ax_size(ax1, fig1)
-            check_answer(width_pix, height_pix,
+            _check_answer(width_pix, height_pix,
                          approx_width_1, approx_height_1)
 
             # Second, create a subplot on that same Figure
@@ -63,6 +84,7 @@ def test_get_ax_size():
                         ax_sub,
                         fig1
                     )
-                    check_answer(correct_width_sub, correct_height_sub,
+                    _check_answer(correct_width_sub, correct_height_sub,
                                  approx_width_sub, approx_height_sub)
 
+# *** END helper objects/tests for _get_ax_size ***
