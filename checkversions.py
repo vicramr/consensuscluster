d163a55c6b3fd3294c50ee25fd88404caa8fbeb6
Vicram Rajagopalan <vicram.r@hotmail.com>
2019-06-06 19:26:56 -0500

    Many more CI additions First of all, matplotlib 2.0 fails because it does not have the function Figure.subplots. I may not actually need this function in practice but I still don't think it's worthwhile to support any version less than 2.2 (which does have this function). Of course, this now means that I have to actually test MPL 2.2 so I have added a MPL_VERSION env variable to Appveyor and Travis. In Appveyor, made the environment variables have the = sign in them so I can customize that for each build. I don't need to yet but I might want to use >= or < at some point. Also numpy 1.13.1 is not available for Python 3.7 so I changed that in Appveyor. Finally, I added a LOT of builds to Travis in order to really test all the nooks and crannies. Travis allows a lot more builds than Appveyor so this is where I can test various combinations of package versions. In order to make my life easier with regards to double-checking package versions, I have also written a Python script which uses regular expressions to verify that numpy, scipy, sklearn, and matplotlib have valid version numbers, and it exits with a nonzero error code if not. I have not tested this script but I think it'll work. Of course I will have to verify that by hand in the upcoming build.

---
+++ checkversions.py
@@ -0,0 +1,39 @@
+import sys
+import re
+
+numpy_version_env = sys.argv[1]
+scipy_version_env = sys.argv[2]
+sklearn_version_env = sys.argv[3]
+mpl_version_env = sys.argv[4]
+
+numpy_regex = numpy_version_env.replace('.', r'\.')
+scipy_regex = scipy_version_env.replace('.', r'\.')
+sklearn_regex = sklearn_version_env.replace('.', r'\.')
+mpl_regex = mpl_version_env.replace('.', r'\.')
+
+numpy_pattern = re.compile(numpy_regex)
+scipy_pattern = re.compile(scipy_regex)
+sklearn_pattern = re.compile(sklearn_regex)
+mpl_pattern = re.compile(mpl_regex)
+
+import numpy
+import scipy
+import sklearn
+import matplotlib as mpl
+
+def match_entire_string(pattern, string):
+  """
+  Return True if pattern matches the entire string, False otherwise.
+  """
+  match = pattern.match(string)
+  if match:
+    return string == match.group(0)  # match.group(0) returns whole string
+  else:
+    return False
+
+if (not match_entire_string(numpy_pattern, numpy.__version__)) or
+   (not match_entire_string(scipy_pattern, scipy.__version__)) or
+   (not match_entire_string(sklearn_pattern, sklearn.__version__)) or
+   (not match_entire_string(mpl_pattern, mpl.__version__)):
+  sys.exit(1)
+print('All versions look correct.')
